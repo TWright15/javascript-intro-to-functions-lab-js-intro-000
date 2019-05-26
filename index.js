30d5f45c8ea84f0e963a96449f8a04ef18293889
Tara Wright <wrightt2015@gmail.com>
2019-05-26 01:26:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  return string("I can't hear you!")
+  var lowercase = "I can't hear you!"
+  return string.toLowerCase(lowercase)
 
 }
