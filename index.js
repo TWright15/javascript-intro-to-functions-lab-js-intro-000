9e6cdc56ca344e56dbfb8b397034cc9cd5ab87a2
Tara Wright <wrightt2015@gmail.com>
2019-05-26 00:55:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function whisper(string){
   return string.toLowerCase()
 }
 
-function logShout(string){
-  console.log('hello'.toUpperCase)
-}
+//function logShout(string){
+  //console.log('hello'.toUpperCase)
+//}
